ab0c7822f8f061ec7e5e967a1ef02d04d3fc300b
JONGGIL LEE <brianlee2k@naver.com>
2022-04-09 13:41:17 +0900

    메인페이지

--- src/elements/Image.js
+++ src/elements/Image.js
@@ -10,7 +10,7 @@ const Image = (props) => {
   if (shape === "circle") {
     return <CircleImage {...styles}></CircleImage>;
   }
-  if (shape === "ractangle") {
+  if (shape === "rectangle") {
     return (
       <RectangleOutter>
         <RectangleInner {...styles}></RectangleInner>
