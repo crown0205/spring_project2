d3b3b18602a9a12b81a92ea9c43fa24d9e8d148f
crown0205 <crown0205@gmail.com>
2022-04-09 13:25:31 +0900

    App.js 수정, PostDetail 추가

---
+++ src/pages/PostDetail.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Grid, Button, Text, Input, Image } from "../elements/index";
+
+const PostDetail = props => {
+  return (
+    <React.Fragment>
+      <Grid bg="#aaa" width="100%" height="100.3vw">
+        {/* header */}
+
+        <Grid margin="50px auto" width="40%" bg="#fff" padding="50px 16px">
+          <Text Fw="600" size="34px" margin="0 0 5px 0">
+            이것은 테스트 하는 곳입니다.
+          </Text>
+          <Text size="14px" color="#8b8b8b">
+            user_name | <span>2022-04-09 10:00:00</span>
+          </Text>
+
+          <Grid margin="50px 0">
+            <Grid>
+              <Image
+                shape="ractangle"
+                src="https://i.pinimg.com/564x/ca/02/94/ca0294f0578167fcdf6df476d4645772.jpg"
+                alt=""
+                width="100%"
+                height="400px"
+              ></Image>
+            </Grid>
+          </Grid>
+
+          <Grid>{/* comtents */}</Grid>
+
+          <Grid width="100%">
+            {/* comments write */}
+            <Input width="80%" left />
+            <Button
+              text="게시"
+              width="20%"
+              padding="10px"
+              height="41.7px"
+            ></Button>
+          </Grid>
+
+          <Grid>
+            {/* comments */}
+            <Grid>
+              <Grid bg="#eeeeee">
+                <Text>user_name</Text>
+              </Grid>
+              <Grid>
+                comment
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid>{/* btn */}</Grid>
+        </Grid>
+
+        {/* footer */}
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+export default PostDetail;
--- src/shared/App.js
+++ src/shared/App.js
@@ -5,15 +5,16 @@ import React from "react";
 
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-// import PostList from "../pages/PostList";
-
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import PostDetail from "../pages/PostDetail";
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
-        {/* <Route path="/" exact component={PostList} /> */}
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
+        <Route path="/detail" exact component={PostDetail} />
       </BrowserRouter>
     </React.Fragment>
   );
