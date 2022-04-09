19f9d587cf0610c0c245191ac8a01f3ce6be9d24
crown0205 <crown0205@gmail.com>
2022-04-09 15:33:00 +0900

    링크 걸었습니다

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,66 @@
 import React from "react";
 
 import { Grid, Text, Button } from "../elements";
+import { useHistory } from "react-router-dom";
 
-const Header = (props) => {
+const Header = props => {
+  const history = useHistory();
+
   return (
     <React.Fragment>
       <Grid bg="#ddd">
         <Grid is_flex padding="3px 16px" width="40%" margin="auto" bg="#ffe0e0">
           <Grid>
             <Text margin="0px" size="25px" bold>
-              올봄엔
+              <span
+                onClick={() => {
+                  console.log("home");
+                  history.replace("/");
+                }}
+              >
+                올봄엔
+              </span>
             </Text>
           </Grid>
           <Grid width="auto" is_flex>
-            <Button text="로그인" width="80px"></Button>
-            <Button text="회원가입" width="80px"></Button>
+            <Button
+              text="로그인"
+              width="80px"
+              _onClick={() => {
+                console.log("됨??");
+                history.push("/login");
+              }}
+            ></Button>
+            <Button
+              text="회원가입"
+              width="80px"
+              _onClick={() => {
+                history.push("/signup");
+              }}
+            ></Button>
           </Grid>
         </Grid>
       </Grid>
     </React.Fragment>
   );
 
-  //   return (
-  //     <React.Fragment>
-  //       <Grid is_flex padding="3px 16px">
-  //         <Grid>
-  //           <Text margin="0px" size="25px" bold>
-  //             올봄엔
-  //           </Text>
-  //         </Grid>
-  //         <Grid width="auto" is_flex>
-  //           <Button text="내 정보" width="80px"></Button>
-  //           <Button text="알림" width="80px"></Button>
-  //           <Button text="로그아웃" width="80px"></Button>
-  //         </Grid>
+  // return (
+  //   <React.Fragment>
+  //     <Grid is_flex padding="3px 16px">
+  //       <Grid>
+  //         <Text margin="0px" size="25px" bold>
+  //           올봄엔
+  //         </Text>
   //       </Grid>
-  //     </React.Fragment>
-  //   );
+  //       <Grid width="auto" is_flex>
+  //         <Button text="글쓰기" width="80px" _onClick={() => {
+  //           history.push("/write")
+  //         }}></Button>
+  //         <Button text="로그아웃" width="80px"></Button>
+  //       </Grid>
+  //     </Grid>
+  //   </React.Fragment>
+  // );
 };
 
 Header.defaultProps = {};
--- src/shared/App.js
+++ src/shared/App.js
@@ -11,18 +11,29 @@ import PostWrite from "../pages/PostWrite";
 import PostDetail from "../pages/PostDetail";
 import Footer from "../components/Footer";
 
+import { Button } from "../elements/index"
+import { useHistory } from "react-router-dom";
+
 function App() {
+  const history = useHistory()
   return (
     <React.Fragment>
-      <Header />
       <BrowserRouter>
+        <Header />
         <Route path="/" exact component={Main} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
-        <Route path="/posts" exact component={PostWrite} />
+        <Route path="/write" exact component={PostWrite} />
         <Route path="/detail" exact component={PostDetail} />
+        <Footer />
+        <Button
+          is_float
+          text="+"
+          _onClick={() => {
+            history.push("/write");
+          }}
+        ></Button>
       </BrowserRouter>
-      <Footer />
     </React.Fragment>
   );
 }
