d54500f413e4bc60cc8fe8122c51bfb2a4130647
kimyeongkyung <klk17625@naver.com>
2022-04-09 13:29:30 +0900

    Header,Footer width값 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,11 @@ import { Grid, Text, Button } from "../elements";
 const Footer = (props) => {
   return (
     <React.Fragment>
-      <Grid is_flex padding="3px 16px">
-        <Button text="F/E 정현수 김영경 이종길"></Button>
-        <Button text="B/E 이현승 배정민 오경은"></Button>
+      <Grid bg="#ddd">
+        <Grid is_flex width="40%" margin="auto">
+          <Button text="F/E 정현수 김영경 이종길"></Button>
+          <Button text="B/E 이현승 배정민 오경은"></Button>
+        </Grid>
       </Grid>
     </React.Fragment>
   );
