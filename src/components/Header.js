42e4f9677bf7adee39b39d691edf607e1a28700a
JONGGIL LEE <brianlee2k@naver.com>
2022-04-09 15:57:21 +0900

    메인페이지 수정  , 헤더 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,46 @@ import React from "react";
 import { Grid, Text, Button } from "../elements";
 import { useHistory } from "react-router-dom";
 
-const Header = props => {
+const Header = (props) => {
   const history = useHistory();
+
+  // return (
+  //   <React.Fragment>
+  //     <Grid bg="#ddd">
+  //       <Grid is_flex padding="3px 16px" width="40%" margin="auto" bg="#ffe0e0">
+  //         <Grid>
+  //           <Text margin="0px" size="25px" bold>
+  //             <span
+  //               onClick={() => {
+  //                 console.log("home");
+  //                 history.replace("/");
+  //               }}
+  //             >
+  //               올봄엔
+  //             </span>
+  //           </Text>
+  //         </Grid>
+  //         <Grid width="auto" is_flex>
+  //           <Button
+  //             text="로그인"
+  //             width="auto"
+  //             _onClick={() => {
+  //               console.log("됨??");
+  //               history.push("/login");
+  //             }}
+  //           ></Button>
+  //           <Button
+  //             text="회원가입"
+  //             width="auto"
+  //             _onClick={() => {
+  //               history.push("/signup");
+  //             }}
+  //           ></Button>
+  //         </Grid>
+  //       </Grid>
+  //     </Grid>
+  //   </React.Fragment>
+  // );
 
   return (
     <React.Fragment>
@@ -24,18 +62,17 @@ const Header = props => {
           </Grid>
           <Grid width="auto" is_flex>
             <Button
-              text="로그인"
-              width="80px"
+              text="글 작성"
+              width="auto"
               _onClick={() => {
-                console.log("됨??");
-                history.push("/login");
+                history.push("/write");
               }}
             ></Button>
             <Button
-              text="회원가입"
-              width="80px"
+              text="로그아웃"
+              width="auto"
               _onClick={() => {
-                history.push("/signup");
+                history.replace("/");
               }}
             ></Button>
           </Grid>
@@ -43,24 +80,6 @@ const Header = props => {
       </Grid>
     </React.Fragment>
   );
-
-  // return (
-  //   <React.Fragment>
-  //     <Grid is_flex padding="3px 16px">
-  //       <Grid>
-  //         <Text margin="0px" size="25px" bold>
-  //           올봄엔
-  //         </Text>
-  //       </Grid>
-  //       <Grid width="auto" is_flex>
-  //         <Button text="글쓰기" width="80px" _onClick={() => {
-  //           history.push("/write")
-  //         }}></Button>
-  //         <Button text="로그아웃" width="80px"></Button>
-  //       </Grid>
-  //     </Grid>
-  //   </React.Fragment>
-  // );
 };
 
 Header.defaultProps = {};
