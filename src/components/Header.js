9abca1ee0f395149aeadca4b0ca2023932a0f19b
crown0205 <crown0205@gmail.com>
2022-04-11 13:09:55 +0900

    header 로그인 하면 버튼 바뀌게 함 & 로그아웃하면 다시 원래 대로 돌아옴

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,47 +3,71 @@ import styled from "styled-components";
 
 import { Grid, Text, Button } from "../elements";
 import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteCookie } from "../shared/Cookie.js";
+import { actionCreators as userActions } from "../redux/modules/user";
 
-const Header = (props) => {
+
+const Header = props => {
   const history = useHistory();
+  const dispatch = useDispatch()
 
-  // return (
-  //   <React.Fragment>
-  //     <Grid bg="#ddd" fixed>
-  //       <Grid is_flex padding="3px 16px" width="40%" margin="auto" bg="#ffe0e0">
-  //         <Grid>
-  //           <Text margin="0px" size="25px" bold>
-  //             <F
-  //               onClick={() => {
-  //                 console.log("home");
-  //                 history.replace("/");
-  //               }}
-  //             >
-  //               올봄엔
-  //             </F>
-  //           </Text>
-  //         </Grid>
-  //         <Grid width="auto" is_flex>
-  //           <Button
-  //             text="로그인"
-  //             width="90px"
-  //             _onClick={() => {
-  //               console.log("됨??");
-  //               history.push("/login");
-  //             }}
-  //           ></Button>
-  //           <Button
-  //             text="회원가입"
-  //             width="90px"
-  //             _onClick={() => {
-  //               history.push("/signup");
-  //             }}
-  //           ></Button>
-  //         </Grid>
-  //       </Grid>
-  //     </Grid>
-  //   </React.Fragment>
-  // );
+  console.log(useSelector(state => state.user.is_login));
+  const is_login = useSelector(state => state.user.is_login);
+  const user_name = useSelector(state => state.user.user);
+
+
+  if (is_login) {
+    return (
+      <React.Fragment>
+        <Grid bg="#ffe0e0" is_flex fixed height="0px">
+          <Grid
+            is_flex
+            padding="3px 16px"
+            width="55%"
+            margin="auto"
+            bg="#ffe0e0"
+          >
+            <Grid>
+              <Font
+                onClick={() => {
+                  console.log("home");
+                  history.replace("/");
+                }}
+              >
+                <span>🌸</span>
+                올봄엔
+                <span>🌸</span>
+              </Font>
+            </Grid>
+            <Grid is_flex>
+              <Grid margin="0px 0px 0px 100px">
+                <Text>{user_name}님 환영합니다</Text>
+              </Grid>
+              <Grid margin="0px 2px 0px 0px">
+                <Button
+                  text="글 작성"
+                  width="90px"
+                  margin="0px 4px 0px 0px"
+                  _onClick={() => {
+                    history.push("/posts");
+                  }}
+                ></Button>
+                <Button
+                  text="로그아웃"
+                  width="90px"
+                  _onClick={() => {
+                    dispatch(userActions.logOutDB())
+                    history.push("/");
+                  }}
+                ></Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 
   return (
     <React.Fragment>
@@ -63,22 +87,19 @@ const Header = (props) => {
           </Grid>
           <Grid is_flex>
             <Grid margin="0px 0px 0px 100px">
-              <Text>user_name님 환영합니다</Text>
-            </Grid>
-            <Grid margin="0px 2px 0px 0px">
               <Button
-                text="글 작성"
+                text="로그인"
                 width="90px"
-                margin="0px 4px 0px 0px"
                 _onClick={() => {
-                  history.push("/posts");
+                  console.log("됨??");
+                  history.push("/login");
                 }}
               ></Button>
               <Button
-                text="로그아웃"
+                text="회원가입"
                 width="90px"
                 _onClick={() => {
-                  history.replace("/");
+                  history.push("/signup");
                 }}
               ></Button>
             </Grid>
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,96 +1,187 @@
-// import { createAction, handleActions } from "redux-actions";
-// import { produce } from "immer";
-// import { getCookie, setCookie, deleteCookie } from "../../shared/Cookie";
-// import { api, api_token , test_api, test_api2} from "../../shared/api";
-// import axios from "axios";
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+// import { config } from "../../shared/config";
+import axios from "axios";
+import { setCookie, deleteCookie, getCookie } from "../../shared/Cookie";
 
-// const GET_USER = "GET_USER";
-// const SET_USER = "SET_USER";
-// const LOG_OUT = "LOG_OUT";
+// 액션 타입
+const LOG_IN = "LOG_IN"; // 로그인
+const LOG_OUT = "LOG_OUT"; // 로그아웃
+const GET_USER = "GET_USER"; //회원정보 조회
+// 액션 생성함수
+const getUser = createAction(GET_USER, (user) => ({ user }));
+const setUser = createAction(LOG_IN, (user) => ({ user }));
+const logOut = createAction(LOG_OUT, (user) => ({ user }));
 
-// const getUser = createAction(GET_USER, (user) => ({ user }));
-// const setUser = createAction(SET_USER, (nickname) => ({ nickname }));
-// const logOut = createAction(LOG_OUT, (user) => ({ user }));
+//  초기값
+// is_login : 로그인 상태인지 아닌지 여부 확인
+const initialState = {
+  user: "dingo",
+  is_login: true, 
+};
 
-// const initialState = {
-//   nickname: null,
-//   is_login: false,
-// };
 
-// const loginFB = (nickname, password) => {
-//   return async (dispatch, getState, { history }) => {
-//     api_token
-//       .post("/user/login", {
-//         nickname: nickname,
-//         password: password,
-//       })
-//       .then((res) => {
-//         localStorage.setItem("is_login", res.data.token);
-//         localStorage.setItem("nickname", res.data.nickname);
-//         localStorage.setItem("userId", res.data.id);
 
-//         dispatch(setUser(nickname));
+
+// axios.get('').then((Response)=>{
+//     console.log(Response.data);
+// }).catch((Error)=>{
+//     console.log(Error);
+// })
+
+
+
+// // 회원가입
+// const signUpDB = (id, user_name, pwd, pwdcheck) => {
+//   return function (dispatch, getState, { history }) {
+
+//     // axios.post("url", {
+//     //   data: {
+//     //     user_id: id,
+//     //     user_name : user_name,
+//     //     pwd: pwd,
+//     //     user_profile: "",
+//     //   }
+//     // })
+
+
+//     axios({
+//       method: "get",
+//       url: `https://6251cd887f7fa1b1dddf398b.mockapi.io/user`,
+//       // 회원가입 시 입력 데이터 보내기(보내기만 하면 끝)
+//       // data: {
+//       //   id: id,
+//       //   password: pwd,
+//       //   user_name: user_name,
+//       // },
+//     })
+//       .then((user) => {
+//         console.log(user)
+
+//         window.alert("회원가입을 축하드립니다!");
+//         // history.push("/login");
 //       })
 //       .catch((err) => {
-//         console.log(err.response)
-//         window.alert(err.response.data.msg)
-
+//         console.log("회원가입 에러", err);
+//         window.alert("이미 중복된 아이디입니다. 확인해주세요");
 //       });
 //   };
 // };
 
-// const signUpFB = (nickname, password, pwConfirm) => {
-
-//   return async (dispatch, getState, { history }) => {
-//     await api_token
-//       .post("/user/signin", {
-//         nickname: nickname,
-//         password: password,
-//         confirmPassword: pwConfirm,
-//       })
+// 로그인
+// const LoginDB = (email, pwd) => {
+//   return function (dispatch, getState, { history }) {
+//     axios({
+//       method: "post",
+//       // url: `${config.api}/login`,
+//       data: {
+//         email: email,
+//         password: pwd,
+//       },
+//     })
 //       .then((res) => {
-//         console.log(res);
-//         window.alert(res.data.msg + '\n 로그인을 진행해주세요☺️')
-//         window.location.reload("/");
+//         // console.log(res.data);
+
+//         // 서버로부터 받은 토큰 변수에 할당
+//         const jwtToken = res.data.result.user.token;
+
+//         // 서버로 부터 받은 토큰을 쿠키에 저장 (key:value 형태)
+//         setCookie("is_login", jwtToken);
+
+//         // 통신 시 헤더에 default 값으로 저장
+//         axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
+
+//         const user = {
+//           email: email,
+//           name: res.data.result.user.name,
+//         };
+//         dispatch(setUser(user));
+//         history.replace("/");
 //       })
 //       .catch((err) => {
-//         window.alert(err.response.data.msg)
+//         window.alert("아이디 혹은 비밀번호가 일치하지 않습니다.");
+//         return;
 //       });
 //   };
 // };
 
-// const logOutFB = () => {
-//   return (dispatch, getState, { history }) => {
-//     dispatch(logOut())
-//     history.push('/')
-//   }
-// }
+// 로그인 후 회원 정보 조회
+// const getUserDB = () => {
+//   return function (dispatch, getState, { history }) {
+//     // 로그인 시 쿠키에 이미 is_login으로 토큰이 저장되어 있기 때문에
+//     const jwtToken = getCookie("is_login");
+//     // console.log(jwtToken);
 
-// export default handleActions(
-//   {
-//     [GET_USER]: (state, action) => produce(state, (draft) => {}),
-//     [SET_USER]: (state, action) =>
-//     produce(state, (draft) => {
-//       draft.nickname = action.payload.nickname;
-//       draft.is_login = true;
-//     }),
-//     [LOG_OUT]: (state, action) =>
-//       produce(state, (draft) => {
-//         localStorage.clear(); //localStorage 비워주는 작업
-//         draft.nickname = null;
-//         draft.is_login = false;
-//       }),
-//   },
-//   initialState
-// );
+//     // 새로고침하면 헤더 default도 날라가기 때문에 다시 토큰을 달아준다.
+//     // 백엔드에서 헤더로 넘어온 Authorization 에서 토큰 값에서 토큰값을 뽑아주기로 함.
+//     axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
 
-// const actionCreators = {
-//   getUser,
-//   setUser,
-//   logOut,
-//   logOutFB,
-//   loginFB,
-//   signUpFB,
+//     // console.log(axios.defaults.headers);
+
+//     axios({
+//       method: "post",
+//       // url: `${config.api}/getUser`,
+//     })
+//       .then((res) => {
+//         // console.log(res);
+//         const user = {
+//           email: res.data.email,
+//           name: res.data.name,
+//           id: res.data.userId,
+//         };
+//         dispatch(getUser(user));
+//       })
+//       .catch((err) => {
+//         console.log("유저정보 조회 에러", err);
+//       });
+//   };
 // };
 
-// export { actionCreators };
+const logOutDB = () => {
+  return function (dispatch, getState, { history }) {
+    console.log("logOut 버튼 눌림")
+
+
+    dispatch(logOut())
+  };
+};
+
+// 리듀서
+export default handleActions(
+  {
+    // [LOG_IN]: (state, action) =>
+    //   produce(state, (draft) => {
+    //     // 로그인시 받은 회원 정보 저장, is_login 상태 true 변경
+    //     draft.user = action.payload.user;
+    //     draft.is_login = true;
+    //   }),
+    [LOG_OUT]: (state, action) =>
+      produce(state, (draft) => {
+        //쿠키 삭제
+        console.log(state)
+        deleteCookie("is_login");
+
+        // 유저정보 삭제 하고 로그인상태 false로 변경
+        draft.user = null;
+        draft.is_login = false;
+      }),
+    // [GET_USER]: (state, action) =>
+    //   produce(state, (draft) => {
+    //     // 로그인상태(is_login) true로 변경
+    //     draft.user = action.payload.user;
+    //     draft.is_login = true;
+    //   }),
+  },
+  initialState
+);
+
+const actionCreators = {
+  setUser,
+  logOut,
+  logOutDB,
+  // LoginDB,
+  // signUpDB,
+  // getUserDB,
+};
+
+export { actionCreators };
